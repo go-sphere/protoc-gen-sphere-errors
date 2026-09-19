a6080503ff25dc1b1677cd8e9511dea28fdf1cb6
tbxark <tbxark@outlook.com>
2026-09-19 18:24:33 +0800

    chore: Bump plugin version to 0.0.3

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
-const version = "0.0.1"
+const version = "0.0.3"
 
 var (
 	showVersion  = flag.Bool("version", false, "print the version and exit")
